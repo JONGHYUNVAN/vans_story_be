a4a7c91090892f0482b13b1d129671e7b5cc5cb8
JONGHYUNVAN <whdgus808@naver.com>
2024-12-03 21:44:02 +0900

    Initial commit

---
+++ src/main/java/blog/vans_story_be/VansStoryBeApplication.java
@@ -0,0 +1,13 @@
+package blog.vans_story_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VansStoryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VansStoryBeApplication.class, args);
+    }
+
+}
