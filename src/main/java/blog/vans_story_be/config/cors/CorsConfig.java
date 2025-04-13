9ad92fc8164e8995c9c463c9bbeb9237b319dfbe
JONGHYUNVAN <whdgus808@naver.com>
2025-04-13 14:44:22 +0900

    문서화 업데이트 및 CORS 설정 개선
    - Javadoc 문서의 여러 HTML 파일을 업데이트하여 최신 정보를 반영.
    - CORS 설정에서 허용된 출처에 localhost 및 프로덕션 도메인 추가.
    - 불필요한 CSS 파일 삭제 및 JavaScript 파일에서 검색 기능 개선.
    - 사용자 관련 API 문서 및 DTO에 대한 설명 추가.

--- src/main/java/blog/vans_story_be/config/cors/CorsConfig.java
+++ src/main/java/blog/vans_story_be/config/cors/CorsConfig.java
@@ -29,7 +29,7 @@ public class CorsConfig {
     @Primary
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://vans-story.com", "http://localhost:3001"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
